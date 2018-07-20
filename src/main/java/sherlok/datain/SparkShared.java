1692217b6b0100bab4a86ad0343cbc8e42a8e530
Izik Golan <izik.golan@att.com>
2018-07-20 11:38:55 +0300

    Add work with DirectStream

---
+++ src/main/java/sherlok/datain/SparkShared.java
@@ -0,0 +1,8 @@
+package sherlok.datain;
+
+public class SparkShared {
+    static final String KEYSPACE       = "activity";
+    static final String TABLE_NAME     = "data_collector";
+    static final String CASSANDRA_HOST = "localhost";
+    static final String CASSANDRA_PORT = "9042";
+}
